f90fe59bc75b37de02ecd2ead10882865d8859ac
LucasAck3rmann <ackermannlucas25@gmail.com>
2023-10-26 21:03:36 -0300

    --v 2.0

--- Js/main.js
+++ Js/main.js
@@ -1,20 +1,33 @@
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    //alert('Elemento não encontrado');
+    console.log("Elemento não encontrado ou seletor inválido");
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-//enquanto, para cada tecla, faça
-
+//para
 for (let i = 0; i < listaDeTeclas.length; i++) {
   const tecla = listaDeTeclas[i];
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`; //template string
 
-  //console.log(idAudio);
-
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-  //console.log(i);
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
